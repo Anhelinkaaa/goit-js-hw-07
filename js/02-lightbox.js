b73c7fda3d26f0756ab192b1732dc2af865f7e86
Anhelinkaaa <bragaangelina94@gmail.com>
2023-08-04 14:41:56 +0300

    Added images to 'ul'

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+function handlerCreateGall (arr) {
+    const liEl = arr.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      width = "250px"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+    
+    gallery.insertAdjacentHTML('beforeend', liEl)
+};
+
+handlerCreateGall(galleryItems)
